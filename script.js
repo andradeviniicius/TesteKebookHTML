cc8efca8ae2fef5d724b28fe793fa4bc4e558094
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-12-17 12:28:11 -0300

    fix/ quick styles

---
+++ script.js
@@ -0,0 +1,16 @@
+const email = document.getElementById("emailInput");
+const password = document.getElementById("passwordInput");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+form.addEventListener("submit", (e) => {
+  let messages = [];
+
+  if (email.value === "" || email.value == null) {
+    messages.push("Email is required");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join("");
+  }
+});
